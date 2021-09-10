f5e181a0d8919b339bf5e6765bf34e43f7a77235
Roman <sugus02@inbox.ru>
2021-09-10 16:27:55 +0300

    create all css files and append btns

--- todo/src/components/SearchPanel/SearchPanel.jsx
+++ todo/src/components/SearchPanel/SearchPanel.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import './SearchPanel.css';
+
 const SearchPanel = () => {
-    return (
-        <input placeholder="Search..."/>
-    );
+  return (
+    <input type="text"
+              className="form-control search-input"
+              placeholder="type to search" />
+  );
 };
 
 export default SearchPanel;
--- todo/src/components/TodoList/TodoList.jsx
+++ todo/src/components/TodoList/TodoList.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import TodoListItem from '../TodoListItem/TodoListItem';
 import './TodoList.css';
+
 const TodoList = ({ todos }) => {
 
-    const elements = todos.map((item) => {
-        return (
-            <li className="list-group-item todo-list-item">
-                <TodoListItem {...item} />
-            </li>
-        )
-    })
+  const elements = todos.map((item) => {
+    const { id, ...itemProps } = item;
+
     return (
-        <ul className="list-group todo-list">
-            {elements}
-        </ul>
+      <li key={id} className="list-group-item">
+        <TodoListItem {...itemProps } />
+      </li>
     );
+  });
+
+  return (
+    <ul className="list-group todo-list">
+      { elements }
+    </ul>
+  );
 };
 
 export default TodoList;
